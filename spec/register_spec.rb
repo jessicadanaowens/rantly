0e153ee81b09e3f3c8726764a753b51627bc0b8b
Jessica Owens <jessicadanaowens@gmail.com>
2014-09-13 20:49:26 -0600

    all tests are passing for user registration

--- spec/register_spec.rb
+++ spec/register_spec.rb
@@ -1,31 +1,37 @@
-# require "rails_helper"
-#
-# feature "User can register" do
-#   scenario "successfully" do
-#     visit root_url
-#
-#     click_on "Join"
-#     fill_in "Username", :with => "jessica"
-#     fill_in "Password", :with => "password"
-#     click_on "Register"
-#
-#     expect(page).to have_content "Thank you for registering"
-#   end
-#
-#   scenario "unsuccessfully" do
-#     visit root_url
-#
-#     click_on "Join"
-#     fill_in "Username", :with => ""
-#     fill_in "First name", :with => "Jessica"
-#     fill_in "Last name", :with => "Owens"
-#     fill_in "Bio", :with => "I love to draw!"
-#     fill_in "Password", :with => "password"
-#     within("#frequency-of-rants-form") do
-#       choose "Daily"
-#     end
-#     click_on "Register"
-#
-#     expect(page).to have_content "Username can't be blank"
-#   end
-# end
+require "rails_helper"
+
+feature "User can register" do
+  scenario "successfully" do
+    visit root_url
+
+    click_on "Join"
+    fill_in "Username", :with => "Jessica"
+    fill_in "First name", :with => "Jessica"
+    fill_in "Last name", :with => "Owens"
+    fill_in "Bio", :with => "I love to draw!"
+    fill_in "Password", :with => "password"
+    within("#frequency-of-rants-form") do
+      choose "user_frequency_daily"
+    end
+    click_on "Register"
+
+    expect(page).to have_content "Thank you for registering"
+  end
+
+  scenario "unsuccessfully" do
+    visit root_url
+
+    click_on "Join"
+    fill_in "Username", :with => ""
+    fill_in "First name", :with => "Jessica"
+    fill_in "Last name", :with => "Owens"
+    fill_in "Bio", :with => "I love to draw!"
+    fill_in "Password", :with => "password"
+    within("#frequency-of-rants-form") do
+      choose "user_frequency_daily"
+    end
+    click_on "Register"
+
+    expect(page).to have_content "Username can't be blank"
+  end
+end
