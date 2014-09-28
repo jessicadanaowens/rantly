542c93d2ef110294887fd6e2941e9bd17354e07c
Jessica Owens <jessicadanaowens@gmail.com>
2014-09-28 17:51:15 -0600

    rants are sorted by favorites

--- app/models/rant.rb
+++ app/models/rant.rb
@@ -1,8 +1,16 @@
 class Rant < ActiveRecord::Base
   belongs_to :user
+  has_many :favorite_rants
 
   validates_presence_of :topic, :rant, :user_id
   validates_length_of :topic, :maximum => 50, :message => "Title must be less than 50 characters"
   validates_length_of :rant, :minimum => 140, :message => "Body must be greater than 140 characters"
+
+  def self.sort_by_favorites(rants)
+
+    rants.sort_by do |rant|
+      FavoriteRant.where(:rant_id => rant.id).length
+    end
+  end
 
 end
