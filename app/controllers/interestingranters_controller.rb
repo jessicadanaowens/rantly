3c68651363cf1f41a317905732e9c1cbf8d4f2b5
Jessica Owens <jessicadanaowens@gmail.com>
2014-10-06 09:12:13 -0600

    styling

--- app/controllers/interestingranters_controller.rb
+++ app/controllers/interestingranters_controller.rb
@@ -2,25 +2,22 @@ class InterestingrantersController < ApplicationController
 
   def index
     @interesting_ranters = current_user.interestingranters
-
-
+    @user = User.find(session[:user_id])
+    @rant = Rant.new
   end
 
   def create
-    p "*"
-    p params
    @interesting_ranter = Interestingranter.new(
      :user_id => params[:user_id],
      :ranter_id =>params["format"]
    )
 
-  if @interesting_ranter.save!
-    flash[:notice] = "Follow was successful"
-    redirect_to :back
-  else
-    redirect_to :back
-  end
-
+    if @interesting_ranter.save!
+      flash[:notice] = "Follow was successful"
+      redirect_to :back
+    else
+      redirect_to :back
+    end
   end
 
   def destroy
