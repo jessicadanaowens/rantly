1e3db5296ad60bf15ac1d8ccb1eda9e6eadced6b
Jessica Owens <jessicadanaowens@gmail.com>
2014-10-27 17:45:11 -0600

    added mailer configurations

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,4 +35,14 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
   config.action_mailer.delivery_method = :test
+
+  ActionMailer::Base.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => "gmail.com",
+    :user_name            => ENV['USERNAME'],
+    :password             => ENV['PASSWORD'],
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+  }
 end
